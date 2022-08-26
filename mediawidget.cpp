651aa25c790d239462e8378fd2c96076189bf0ff
tongwen_jie <tongwen_jie@163.com>
2022-08-26 14:53:02 +0800

    0826初始文件

---
+++ mediawidget.cpp
@@ -0,0 +1,14 @@
+#include "mediawidget.h"
+#include "ui_mediawidget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
---
+++ mediawidget.h
@@ -0,0 +1,22 @@
+#ifndef MEDIAWIDGET_H
+#define MEDIAWIDGET_H
+
+#include <QWidget>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private:
+    Ui::Widget *ui;
+};
+
+#endif // MEDIAWIDGET_H
