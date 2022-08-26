2bc01738c105a1b75f7f8ba4e5cd60dc9643aa57
tongwen_jie <tongwen_jie@163.com>
2022-08-26 17:02:45 +0800

    0826最简单没有什么功能播放器

--- mediawidget.cpp
+++ mediawidget.cpp
@@ -5,10 +5,46 @@ Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
+
     ui->setupUi(this);
+    setWindowTitle("某不愿透露姓名的播放器");
+    mediaplayer=new QMediaPlayer;
+    mediaplaylist=new QMediaPlaylist;
+    videowidget=new QVideoWidget(ui->label);
+    mediaplayer->setPlaylist(mediaplaylist);
+    mediaplayer->setVideoOutput(videowidget);
 }
 
 Widget::~Widget()
 {
     delete ui;
+}
+
+void Widget::on_pushButton_clicked()
+{
+    QStringList filenames = QFileDialog::getOpenFileNames(this,"打开文件","C:/Users/tongw/Desktop","allfiles(*.*);;""mp3(*.mp3);;""mp4(*.mp4);;""mkv(*.mkv)");
+    mediaplaylist->clear();
+    foreach(QString const & name,filenames)
+    {
+        QUrl url(name);
+        mediaplaylist->addMedia(url);
+
+    }
+    i=1;
+}
+
+void Widget::on_pushButton_2_clicked()
+{
+    if(i==1)
+    {
+        mediaplayer->play();
+        videowidget->resize(ui->label->size());
+        i=0;
+    }
+    else if(i==0)
+    {
+        mediaplayer->pause();
+        videowidget->resize(ui->label->size());
+        i=1;
+    }
 }
--- mediawidget.h
+++ mediawidget.h
@@ -1,6 +1,10 @@
 #ifndef MEDIAWIDGET_H
 #define MEDIAWIDGET_H
-
+#include <QFileDialog>
+#include <QDebug>
+#include <QMediaPlayer>
+#include <QMediaPlaylist>
+#include <QVideoWidget>
 #include <QWidget>
 
 namespace Ui {
@@ -15,8 +19,17 @@ public:
     explicit Widget(QWidget *parent = nullptr);
     ~Widget();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
 private:
     Ui::Widget *ui;
+    QMediaPlayer* mediaplayer;
+    QMediaPlaylist* mediaplaylist;
+    QVideoWidget* videowidget;
+    int i=1;
 };
 
 #endif // MEDIAWIDGET_H
